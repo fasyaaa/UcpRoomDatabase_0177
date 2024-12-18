e6dcf9705e5bfb66e13f8b7086fa0b6e7d8972e4
fasyaaa <muhammad.a.f.2210@gmail.com>
2024-12-19 04:35:27 +0700

    ADD - Tambah fungsi untuk menyimpan input form ke dalam entity dokter

--- app/src/main/java/com/example/ucp2/ui/viewmodel/dokter/DokterViewModel.kt
+++ app/src/main/java/com/example/ucp2/ui/viewmodel/dokter/DokterViewModel.kt
@@ -1,5 +1,7 @@
 package com.example.ucp2.ui.viewmodel.dokter
 
+import com.example.ucp2.data.entity.Dokter
+
 data class DokterEvent(
     val idDkr: String = "",
     val nama: String = "",
@@ -7,4 +9,13 @@ data class DokterEvent(
     val klinik : String = "",
     val nohp : String = "",
     val jamkerja : String = ""
+)
+
+fun DokterEvent.toDokterEntity(): Dokter = Dokter(
+    idDokter = idDkr,
+    nama = nama,
+    spesialis = spesialis,
+    klinik = klinik,
+    nohp = nohp,
+    jamkerja = jamkerja
 )
