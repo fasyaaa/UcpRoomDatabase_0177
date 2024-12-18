e5e4f1df06fecbb45c1d8592517ff33cb8edc741
fasyaaa <muhammad.a.f.2210@gmail.com>
2024-12-19 02:40:20 +0700

    ADD - Implementasi InterfaceContainerApp dan ContainerApp untuk Dependency Injection

--- app/src/main/java/com/example/ucp2/dependenciesinjection/ContainerApp.kt
+++ app/src/main/java/com/example/ucp2/dependenciesinjection/ContainerApp.kt
@@ -1,2 +1,22 @@
 package com.example.ucp2.dependenciesinjection
 
+import android.content.Context
+import com.example.ucp2.data.database.RsDatabase
+import com.example.ucp2.repository.LocalRepositoryDkr
+import com.example.ucp2.repository.LocalRepositoryJdw
+import com.example.ucp2.repository.RepositoryDkr
+import com.example.ucp2.repository.RepositoryJdw
+
+interface InterfaceContainerApp{
+    val repositoryDkr: RepositoryDkr
+    val repositoryJdw: RepositoryJdw
+}
+
+class ContainerApp(private val context: Context): InterfaceContainerApp{
+    override val repositoryDkr: RepositoryDkr by lazy {
+        LocalRepositoryDkr(RsDatabase.getDatabase(context).dokterDao())
+    }
+    override val repositoryJdw: RepositoryJdw by lazy {
+        LocalRepositoryJdw(RsDatabase.getDatabase(context).jadwalDao())
+    }
+}
