126792176c08fede1f41b973d454144a975210e5
fasyaaa <muhammad.a.f.2210@gmail.com>
2024-12-19 04:31:55 +0700

    ADD - Tambah data class DokterEvent untuk representasi data dokter

--- app/src/main/java/com/example/ucp2/ui/viewmodel/dokter/DokterViewModel.kt
+++ app/src/main/java/com/example/ucp2/ui/viewmodel/dokter/DokterViewModel.kt
@@ -1,2 +1,10 @@
 package com.example.ucp2.ui.viewmodel.dokter
 
+data class DokterEvent(
+    val idDkr: String = "",
+    val nama: String = "",
+    val spesialis : String = "",
+    val klinik : String = "",
+    val nohp : String = "",
+    val jamkerja : String = ""
+)
