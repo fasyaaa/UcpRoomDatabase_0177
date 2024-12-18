a4357c1fe878a5f0584a5b4988f1612a211c16d1
fasyaaa <muhammad.a.f.2210@gmail.com>
2024-12-18 17:53:03 +0700

    ADD - methode untuk insert, read data dokter

--- app/src/main/java/com/example/ucp2/repository/LocalRepositoryDkr.kt
+++ app/src/main/java/com/example/ucp2/repository/LocalRepositoryDkr.kt
@@ -1,4 +1,15 @@
 package com.example.ucp2.repository
 
-class LocalRepositoryDkr {
+import com.example.ucp2.data.dao.DokterDao
+import com.example.ucp2.data.entity.Dokter
+import kotlinx.coroutines.flow.Flow
+
+class LocalRepositoryDkr(private val dokterDao: DokterDao): RepositoryDkr {
+    override suspend fun insertDkr(dokter: Dokter) {
+        dokterDao.InsertDokter(dokter)
+    }
+
+    override fun getAllDkr(): Flow<List<Dokter>> {
+        return (dokterDao.getAllDokter())
+    }
 }
