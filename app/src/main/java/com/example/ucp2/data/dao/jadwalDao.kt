03ce746f87cc759fe2bab4c2cda264396a1a1e8b
fasyaaa <muhammad.a.f.2210@gmail.com>
2024-12-18 17:25:52 +0700

    ADD/DEL - tambah bagian CRUD jadwalDao.kt + hapus file database dokterDb.kt dan jadwalDb.kt

--- app/src/main/java/com/example/ucp2/data/dao/jadwalDao.kt
+++ app/src/main/java/com/example/ucp2/data/dao/jadwalDao.kt
@@ -1,4 +1,32 @@
 package com.example.ucp2.data.dao
 
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.Query
+import androidx.room.Update
+import com.example.ucp2.data.entity.Dokter
+import com.example.ucp2.data.entity.Jadwal
+import kotlinx.coroutines.flow.Flow
+
+@Dao
 interface jadwalDao {
+    @Insert
+    suspend fun insertJadwal(
+        jadwal: Jadwal
+    )
+    @Update
+    suspend fun updateJadwal(
+        jadwal: Jadwal
+    )
+    @Delete
+    suspend fun deleteJadwal(
+        jadwal: Jadwal
+    )
+
+    @Query("SELECT * FROM jadwal ORDER BY tglKon ASC")
+    fun getAllJadwal(): Flow<List<Jadwal>>
+
+    @Query("SELECT * FROM jadwal WHERE idJadwal = :idJadwal")
+    fun getJadwal(idJadwal: String): Flow<Jadwal>
 }
--- app/src/main/java/com/example/ucp2/data/database/jadwalDb.kt
+++
@@ -1,2 +0,0 @@
-package com.example.ucp2.data.database
-
