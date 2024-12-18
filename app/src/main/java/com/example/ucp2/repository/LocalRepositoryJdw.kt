2f4af118f133d9705fb46fbe4223928df13586e1
fasyaaa <muhammad.a.f.2210@gmail.com>
2024-12-18 17:55:39 +0700

    ADD - methode untuk insert, update, delete, read data jadwal

--- app/src/main/java/com/example/ucp2/repository/LocalRepositoryJdw.kt
+++ app/src/main/java/com/example/ucp2/repository/LocalRepositoryJdw.kt
@@ -1,4 +1,27 @@
 package com.example.ucp2.repository
 
-class LocalRepositoryJdw {
+import com.example.ucp2.data.dao.JadwalDao
+import com.example.ucp2.data.entity.Jadwal
+import kotlinx.coroutines.flow.Flow
+
+class LocalRepositoryJdw(private val jadwalDao: JadwalDao): RepositoryJdw {
+    override suspend fun insertJdw(jadwal: Jadwal) {
+        jadwalDao.insertJadwal(jadwal)
+    }
+
+    override suspend fun updateJdw(jadwal: Jadwal) {
+        jadwalDao.updateJadwal(jadwal)
+    }
+
+    override suspend fun deleteJdw(jadwal: Jadwal) {
+        jadwalDao.deleteJadwal(jadwal)
+    }
+
+    override fun getAllDkr(): Flow<List<Jadwal>> {
+        return (jadwalDao.getAllJadwal())
+    }
+
+    override fun getMhs(idJdw: String): Flow<Jadwal> {
+        return (jadwalDao.getJadwal(idJdw))
+    }
 }
