24025e5bd0d0da140df9f63b55e7b7f6aa975292
fasyaaa <muhammad.a.f.2210@gmail.com>
2024-12-18 17:44:18 +0700

    ADD - methode ambil data dan masukan data dokter

--- app/src/main/java/com/example/ucp2/repository/RepositoryDkr.kt
+++ app/src/main/java/com/example/ucp2/repository/RepositoryDkr.kt
@@ -1,4 +1,10 @@
 package com.example.ucp2.repository
 
+import com.example.ucp2.data.entity.Dokter
+import kotlinx.coroutines.flow.Flow
+
 interface RepositoryDkr {
+    suspend fun insertDkr(dokter: Dokter)
+
+    fun getAllDkr(): Flow<List<Dokter>>
 }
