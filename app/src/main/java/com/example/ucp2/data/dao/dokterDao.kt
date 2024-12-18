ff15d5f42b4ce3a155ffbe49ad888e6b19ca2170
fasyaaa <muhammad.a.f.2210@gmail.com>
2024-12-18 17:17:26 +0700

    ADD - menyelesaikan dokterDao.kt

--- app/src/main/java/com/example/ucp2/data/dao/dokterDao.kt
+++ app/src/main/java/com/example/ucp2/data/dao/dokterDao.kt
@@ -1,4 +1,18 @@
 package com.example.ucp2.data.dao
 
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.Query
+import com.example.ucp2.data.entity.Dokter
+import kotlinx.coroutines.flow.Flow
+
+@Dao
 interface dokterDao {
+    @Insert
+    suspend fun InsertDokter(
+        dokter: Dokter
+    )
+    @Query("SELECT * FROM dokter ORDER BY nama ASC")
+    fun getAllDokter(): Flow<List<Dokter>>
+
 }
