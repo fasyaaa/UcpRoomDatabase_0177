5ad83232c1a2defc16b1a1f53f42874714baad61
fasyaaa <muhammad.a.f.2210@gmail.com>
2024-12-18 17:46:00 +0700

    ADD - methode ambil data dan masukan data jadwal

--- app/src/main/java/com/example/ucp2/repository/RepositoryJdw.kt
+++ app/src/main/java/com/example/ucp2/repository/RepositoryJdw.kt
@@ -1,4 +1,16 @@
 package com.example.ucp2.repository
 
+import com.example.ucp2.data.entity.Jadwal
+import kotlinx.coroutines.flow.Flow
+
 interface RepositoryJdw {
+    suspend fun insertJdw(jadwal: Jadwal)
+
+    suspend fun updateJdw(jadwal: Jadwal)
+
+    suspend fun deleteJdw(jadwal: Jadwal)
+
+    fun getAllDkr(): Flow<List<Jadwal>>
+
+    fun getMhs(idJdw: String): Flow<Jadwal>
 }
