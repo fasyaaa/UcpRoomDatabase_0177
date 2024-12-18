72a75a0633324a7b18f82c564204ed4497d4d36b
fasyaaa <muhammad.a.f.2210@gmail.com>
2024-12-19 02:46:52 +0700

    ADD - Tambahkan RsSejahteraApp sebagai Application untuk inisialisasi ContainerApp

--- app/src/main/java/com/example/ucp2/RsSejahteraApp.kt
+++ app/src/main/java/com/example/ucp2/RsSejahteraApp.kt
@@ -1,2 +1,13 @@
 package com.example.ucp2
 
+import android.app.Application
+import com.example.ucp2.dependenciesinjection.ContainerApp
+
+class RsSejahteraApp : Application(){
+    lateinit var containerApp: ContainerApp
+
+    override fun onCreate() {
+        super.onCreate()
+        containerApp = ContainerApp(this)
+    }
+}
