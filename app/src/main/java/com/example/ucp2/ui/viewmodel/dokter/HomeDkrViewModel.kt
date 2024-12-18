5cd83f2fcc3c8be0c28ff32092227b7ef54cc91c
fasyaaa <muhammad.a.f.2210@gmail.com>
2024-12-19 04:12:00 +0700

    ADD - Tambah HomeDkrViewModel untuk pengelolaan UI state dan data dokter

--- app/src/main/java/com/example/ucp2/ui/viewmodel/dokter/HomeDkrViewModel.kt
+++ app/src/main/java/com/example/ucp2/ui/viewmodel/dokter/HomeDkrViewModel.kt
@@ -1,2 +1,53 @@
 package com.example.ucp2.ui.viewmodel.dokter
 
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.ucp2.data.entity.Dokter
+import com.example.ucp2.repository.RepositoryDkr
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.filterNotNull
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.onStart
+import kotlinx.coroutines.flow.stateIn
+
+class HomeDkrViewModel (
+    private val repositoryDkr: RepositoryDkr
+): ViewModel(){
+    val homeUiState: StateFlow<HomeUiState> = repositoryDkr
+        .getAllDkr()
+        .filterNotNull()
+        .map {
+            HomeUiState(listDkr = it.toList(),
+                isLoading = false)
+        }
+        .onStart {
+            emit(HomeUiState(isLoading = true))
+            delay(900)
+        }
+        .catch {
+            emit(
+                HomeUiState(
+                    isLoading = false,
+                    isError = true,
+                    errorMessage = it.message ?: "Terjadi Kesalahan"
+                )
+            )
+        }
+        .stateIn(
+            scope = viewModelScope,
+            started = SharingStarted.WhileSubscribed(5000),
+            initialValue = HomeUiState(
+                isLoading = true
+            )
+        )
+}
+
+data class HomeUiState(
+    val listDkr: List<Dokter> = listOf(),
+    val isLoading: Boolean = false,
+    val isError: Boolean = false,
+    val errorMessage: String = ""
+)
