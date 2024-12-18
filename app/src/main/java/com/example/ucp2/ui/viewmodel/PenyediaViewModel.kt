987fa83a7c1da70fb069242919eca9a1b2adf1a7
fasyaaa <muhammad.a.f.2210@gmail.com>
2024-12-19 04:20:58 +0700

    ADD - Tambah PenyediaViewModel untuk inisialisasi ViewModel bagian HomeDkrViewModel

--- app/src/main/java/com/example/ucp2/ui/viewmodel/PenyediaViewModel.kt
+++ app/src/main/java/com/example/ucp2/ui/viewmodel/PenyediaViewModel.kt
@@ -1,2 +1,22 @@
 package com.example.ucp2.ui.viewmodel
 
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewmodel.CreationExtras
+import androidx.lifecycle.viewmodel.initializer
+import androidx.lifecycle.viewmodel.viewModelFactory
+import com.example.ucp2.RsSejahteraApp
+import com.example.ucp2.ui.viewmodel.dokter.HomeDkrViewModel
+
+object PenyediaViewModel {
+
+    val Factory = viewModelFactory {
+        initializer {
+            HomeDkrViewModel(
+                rsSejahtera().containerApp.repositoryDkr
+            )
+        }
+    }
+}
+
+fun CreationExtras.rsSejahtera(): RsSejahteraApp =
+    (this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY] as RsSejahteraApp)
