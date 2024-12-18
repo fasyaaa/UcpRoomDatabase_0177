123edc30d1cc1516d667076062a419aef728a569
fasyaaa <muhammad.a.f.2210@gmail.com>
2024-12-18 17:38:18 +0700

    ADD - tambah pembuatan database

--- app/src/main/java/com/example/ucp2/data/database/RsDatabase.kt
+++ app/src/main/java/com/example/ucp2/data/database/RsDatabase.kt
@@ -1,6 +1,8 @@
 package com.example.ucp2.data.database
 
+import android.content.Context
 import androidx.room.Database
+import androidx.room.Room
 import androidx.room.RoomDatabase
 import com.example.ucp2.data.dao.DokterDao
 import com.example.ucp2.data.dao.JadwalDao
@@ -12,5 +14,21 @@ abstract class RsDatabase: RoomDatabase(){
 
     abstract fun dokterDao(): DokterDao
     abstract fun jadwalDao(): JadwalDao
+
+    companion object{
+        @Volatile
+        private var Instance: RsDatabase? = null
+
+        fun getDatabase(context: Context): RsDatabase{
+            return(Instance ?: synchronized(this){
+                Room.databaseBuilder(
+                    context,
+                    RsDatabase::class.java,
+                    "RsDatabase"
+                )
+                    .build().also { Instance = it }
+            })
+        }
+    }
 }
 
