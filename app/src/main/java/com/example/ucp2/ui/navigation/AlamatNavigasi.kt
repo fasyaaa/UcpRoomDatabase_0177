e039cc13258f1fe613f00564a984ce5627c2621f
fasyaaa <muhammad.a.f.2210@gmail.com>
2024-12-19 03:35:58 +0700

    ADD - Tambahkan implementasi AlamatNavigasi untuk pengaturan rute navigasi

--- app/src/main/java/com/example/ucp2/ui/navigation/AlamatNavigasi.kt
+++ app/src/main/java/com/example/ucp2/ui/navigation/AlamatNavigasi.kt
@@ -1,5 +1,21 @@
 package com.example.ucp2.ui.navigation
 
 interface AlamatNavigasi {
-    
+    val route: String
+}
+
+object DestinasiHome: AlamatNavigasi{
+    override val route = "home"
+}
+
+object DestinasiDetailJdw: AlamatNavigasi{
+    override val route = "detail"
+    const val idJdw = "idJadwal"
+    val routesWithArg = "$route/{$idJdw}"
+}
+
+object DestinasiUpdateJdw: AlamatNavigasi{
+    override val route = "update"
+    const val idJdw = "idJadwal"
+    val routesWithArg = "$route/{$idJdw}"
 }
