603e558184fbde4dde47a5b83f7bd0cc2e64a1a0
fasyaaa <muhammad.a.f.2210@gmail.com>
2024-12-19 12:21:54 +0700

    ADD - Tambah data clas untuk representasi data jadwal

--- app/src/main/java/com/example/ucp2/ui/viewmodel/jadwal/JadwalViewModel.kt
+++ app/src/main/java/com/example/ucp2/ui/viewmodel/jadwal/JadwalViewModel.kt
@@ -1,2 +1,11 @@
 package com.example.ucp2.ui.viewmodel.jadwal
 
+data class JadwalEvent(
+    val idJdw: String = "",
+    val namDkr: String = "",
+    val namPs: String = "",
+    val noHp: String = "",
+    val tglKon: String = "",
+    val status: String = ""
+)
+
