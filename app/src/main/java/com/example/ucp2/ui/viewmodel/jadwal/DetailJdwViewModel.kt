09231fc81062e77b7d837ca0219ae0299c95360e
fasyaaa <muhammad.a.f.2210@gmail.com>
2024-12-21 11:19:19 +0700

    ADD - Tambah function Jadwal dari entity ke ui

--- app/src/main/java/com/example/ucp2/ui/viewmodel/jadwal/DetailJdwViewModel.kt
+++ app/src/main/java/com/example/ucp2/ui/viewmodel/jadwal/DetailJdwViewModel.kt
@@ -1,2 +1,14 @@
 package com.example.ucp2.ui.viewmodel.jadwal
 
+import com.example.ucp2.data.entity.Jadwal
+
+fun Jadwal.toDetailUiEvent() : JadwalEvent{
+    return  JadwalEvent(
+        idJdw = idJadwal,
+        namDkr = namaDokter,
+        namPs = namaPasien,
+        noHp = nohp,
+        tglKon = tglKon,
+        status = status
+    )
+}
