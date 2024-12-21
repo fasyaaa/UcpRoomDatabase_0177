f59e72239cda22e0970e0c29a5c6ed7571a35f1d
fasyaaa <muhammad.a.f.2210@gmail.com>
2024-12-21 11:23:09 +0700

    ADD - Tambah data class DetailUiState

--- app/src/main/java/com/example/ucp2/ui/viewmodel/jadwal/DetailJdwViewModel.kt
+++ app/src/main/java/com/example/ucp2/ui/viewmodel/jadwal/DetailJdwViewModel.kt
@@ -2,6 +2,13 @@ package com.example.ucp2.ui.viewmodel.jadwal
 
 import com.example.ucp2.data.entity.Jadwal
 
+data class DetailUiState(
+    val detailUiEvent: JadwalEvent = JadwalEvent(),
+    val isLoading: Boolean = false,
+    val isError: Boolean = false,
+    val errorMessage: String = ""
+)
+
 fun Jadwal.toDetailUiEvent() : JadwalEvent{
     return  JadwalEvent(
         idJdw = idJadwal,
