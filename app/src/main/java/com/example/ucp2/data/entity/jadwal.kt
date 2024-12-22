7379989d3a1b68726ae36c6b83720916492cf9db
fasyaaa <muhammad.a.f.2210@gmail.com>
2024-12-22 20:11:36 +0700

    FIX - error tipe table idJadwal

--- app/src/main/java/com/example/ucp2/data/entity/jadwal.kt
+++ app/src/main/java/com/example/ucp2/data/entity/jadwal.kt
@@ -6,7 +6,7 @@ import androidx.room.PrimaryKey
 @Entity(tableName = "jadwal")
 data class Jadwal(
     @PrimaryKey(autoGenerate = true)
-    val idJadwal : String,
+    val idJadwal : Int,
     val namaDokter : String,
     val namaPasien : String,
     val nohp : String,
