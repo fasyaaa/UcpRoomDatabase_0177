b79de9aaab733dbc59e1e4961129590c7261c417
fasyaaa <muhammad.a.f.2210@gmail.com>
2024-12-22 21:40:47 +0700

    ADD - gambar untuk splash, styling design

--- app/src/main/java/com/example/ucp2/ui/view/Rs/RsSplashView.kt
+++ app/src/main/java/com/example/ucp2/ui/view/Rs/RsSplashView.kt
@@ -9,17 +9,22 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.ucp2.R
 
 @Composable
@@ -37,6 +42,16 @@ fun RsSplashView(
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
+        Text(
+            text = "Welcome to Sejahtera\nHospital",
+            textAlign = TextAlign.Center,
+            color = Color.White,
+            fontSize = 50.sp,
+            fontWeight = FontWeight.Bold,
+            lineHeight = 60.sp,
+            modifier = Modifier
+                .padding(16.dp)
+        )
         Image(
             painter = painterResource(
                 id = R.drawable.logo
@@ -44,26 +59,31 @@ fun RsSplashView(
             contentDescription = "",
             contentScale = ContentScale.Crop,
             modifier = Modifier
-                .size(150.dp)
+                .size(300.dp)
                 .clip(CircleShape)
                 .background(color = colorResource(id = R.color.background))
         )
-        Spacer(modifier = Modifier.padding(16.dp))
         Text(
             text = "Your Health\nOur Commitment",
             textAlign = TextAlign.Center,
-            modifier = Modifier.padding(16.dp)
+            fontSize = 20.sp,
+            lineHeight = 30.sp,
+            color = Color.White,
+            modifier = Modifier
+                .padding(16.dp)
         )
         Spacer(modifier = Modifier.padding(16.dp))
         Button(
             onClick = {
-                onMulaiButton()
-            }
+                onMulaiButton(
+                )
+            },
+            colors = ButtonDefaults.buttonColors(colorResource(R.color.button)),
+            modifier = Modifier
+                .size(width = 150.dp, height = 50.dp)
         ) {
             Text(text = "Start",
-                modifier = Modifier
-                    .background(color = colorResource(id = R.color.button))
-            )
+                fontSize = 20.sp)
         }
     }
 }
