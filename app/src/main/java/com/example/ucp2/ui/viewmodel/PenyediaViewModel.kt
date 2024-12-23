05bb94730878097adf5e77492ebbbff21d6a7425
fasyaaa <muhammad.a.f.2210@gmail.com>
2024-12-24 05:04:56 +0700

    UPD - Ganti nama

--- app/src/main/java/com/example/ucp2/ui/viewmodel/PenyediaViewModel.kt
+++ app/src/main/java/com/example/ucp2/ui/viewmodel/PenyediaViewModel.kt
@@ -6,24 +6,24 @@ import androidx.lifecycle.viewmodel.CreationExtras
 import androidx.lifecycle.viewmodel.initializer
 import androidx.lifecycle.viewmodel.viewModelFactory
 import com.example.ucp2.RsSejahteraApp
-import com.example.ucp2.ui.viewmodel.dokter.DokterViewModel
+import com.example.ucp2.ui.viewmodel.dokter.AddDokterViewModel
 import com.example.ucp2.ui.viewmodel.dokter.HomeDkrViewModel
+import com.example.ucp2.ui.viewmodel.jadwal.AddJadwalViewModel
 import com.example.ucp2.ui.viewmodel.jadwal.DetailJdwViewModel
 import com.example.ucp2.ui.viewmodel.jadwal.HomeJdwViewModel
-import com.example.ucp2.ui.viewmodel.jadwal.JadwalViewModel
 import com.example.ucp2.ui.viewmodel.jadwal.UpdateJdwViewModel
 
 object PenyediaViewModel {
 
     val Factory = viewModelFactory {
         initializer {
-            DokterViewModel(
+            AddDokterViewModel(
                 rsSejahteraApp().containerApp.repositoryDkr
             )
         }
 
         initializer {
-            JadwalViewModel(
+            AddJadwalViewModel(
                 rsSejahteraApp().containerApp.repositoryJdw,
                 rsSejahteraApp().containerApp.repositoryDkr
             )
